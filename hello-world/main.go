876361f500855c2e68092b0270265bcf1ae7dd21
ristafn <golemmite23@gmail.com>
2022-01-25 22:21:31 +0700

    add comments

---
+++ hello-world/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  // comments
+  // show output Hello World
+	fmt.Println("Hello World")
+  /*
+  multiline comment
+  show output How are you?
+  */
+  fmt.Println("How", "are", "you?")
+}
