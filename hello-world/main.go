c003ab03781ec7205f68885fe323a7fb5b2dd59b
ristafn <golemmite23@gmail.com>
2022-03-23 21:19:06 +0700

    feat: add interface

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,14 +1,53 @@
 package main
 
 import (
-	f "fmt" // aliasing
-
-	. "github.com/ristafn/hello-world/library" // make library same level with main.go
+	"fmt"
+	"math"
 )
 
-func main() {
-	var student = Student{Name: "Ethan", Grade: 21}
+type hitung interface {
+	luas() float64
+	keliling() float64
+}
 
-	f.Println(student.Name)
-	f.Println(student.Grade)
+type lingkaran struct {
+	diameter float64
+}
+
+func (l lingkaran) jari() float64 {
+	return l.diameter / 2
+}
+
+func (l lingkaran) luas() float64 {
+	return math.Pi * math.Pow(l.jari(), 2)
+}
+
+func (l lingkaran) keliling() float64 {
+	return 2 * math.Pi * l.jari()
+}
+
+type persegi struct {
+	sisi float64
+}
+
+func (p persegi) luas() float64 {
+	return math.Pow(p.sisi, 2)
+}
+
+func (p persegi) keliling() float64 {
+	return 4 * p.sisi
+}
+
+func main() {
+	var b hitung
+
+	b = lingkaran{14.0}
+
+	fmt.Println(b.luas())
+	fmt.Println(b.keliling())
+	fmt.Println(b.(lingkaran).jari())
+
+	b = persegi{10.0}
+	fmt.Println(b.luas())
+	fmt.Println(b.keliling())
 }
