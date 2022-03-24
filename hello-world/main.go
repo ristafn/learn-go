a470663501fcc5d55e2244f69863ab2e55c01384
ristafn <golemmite23@gmail.com>
2022-03-24 14:09:50 +0700

    feat: add empty interface

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,53 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-type hitung interface {
-	luas() float64
-	keliling() float64
-}
-
-type lingkaran struct {
-	diameter float64
-}
-
-func (l lingkaran) jari() float64 {
-	return l.diameter / 2
-}
-
-func (l lingkaran) luas() float64 {
-	return math.Pi * math.Pow(l.jari(), 2)
-}
-
-func (l lingkaran) keliling() float64 {
-	return 2 * math.Pi * l.jari()
-}
-
-type persegi struct {
-	sisi float64
-}
-
-func (p persegi) luas() float64 {
-	return math.Pow(p.sisi, 2)
-}
-
-func (p persegi) keliling() float64 {
-	return 4 * p.sisi
-}
+import "fmt"
 
 func main() {
-	var b hitung
+	var secret interface{}
 
-	b = lingkaran{14.0}
+	secret = "ethan hunt"
+	fmt.Println(secret)
 
-	fmt.Println(b.luas())
-	fmt.Println(b.keliling())
-	fmt.Println(b.(lingkaran).jari())
+	secret = []string{"apple", "manggo", "banana"}
+	fmt.Println(secret)
 
-	b = persegi{10.0}
-	fmt.Println(b.luas())
-	fmt.Println(b.keliling())
+	secret = 12.4
+	fmt.Println(secret)
 }
